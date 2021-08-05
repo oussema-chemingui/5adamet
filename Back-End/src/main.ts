4e7cef9d49a983cb331f751418a95f408f9ef767
anis-hannachi <hannachi.anis@gmail.com>
2021-08-05 01:31:14 +0100

    CSS refactoring

--- Back-End/src/main.ts
+++ Back-End/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port =  process.env.PORT;
+  const port = process.env.PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
